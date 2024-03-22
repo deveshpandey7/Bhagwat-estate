7d35dec2bf652f70d614f1d7318c8201c71139f0
Devesh Pandey <deveshpandey607@gmail.com>
2024-03-22 18:48:11 +0530

    working on header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { FaSearch } from 'react-icons/fa';
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+          <span className='text-slate-500'>Bhagwat</span>
+          <span className='text-slate-700'>Estate</span>
+        </h1>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center' >
+          <input type='text' placeholder='Search...' className='bg-transparent' />
+          <FaSearch className='text-slate-500'/>
+        </form>
+      </div>
+    </header>
+  )
+}
