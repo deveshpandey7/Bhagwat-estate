93578c7b4bf544f5bdbffd27a6c16f8198f8aac3
Devesh Pandey <deveshpandey607@gmail.com>
2024-04-04 16:34:54 +0530

    create and add google auth functioanlity

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,3 +45,45 @@ export const signin = async (req, res, next) => {
         next(error);
     }
 }
+
+export const google = async (req, res, next) => {
+    try {
+        // find if user exists
+        const user = await User.findOne({ email: req.body.email });
+
+        // if exists sign in the user
+        if (user){
+             // sign token
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET );
+
+                //  user info without password
+            const { password: pass, ...rest }  = user._doc;
+
+            //and save cookie of user in browser and send
+            res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+          }
+
+          // otherwise sign up the user ie make one new
+          else{
+
+           // make random password coz from google we dont get one
+           const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+           const hashedPasssword = bcryptjs.hashSync(generatedPassword, 10);
+
+           const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPasssword, avatar: req.body.photo });
+
+           await newUser.save();
+
+           const token = jwt.sign({ id: newUser._id, }, process.env.JWT_SECRET );
+
+           const { password: pass, ...rest }  = newUser._doc;
+
+           res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+
+          }
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    avatar: {
+        type: String,
+        default: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F5%2FProfile.png&f=1&nofb=1&ipt=076d617ebd29f2fd9443abd296c510d3a92af06168a4e6520396687f342bb361&ipo=images",
+    },
 }, { timestamps: true }); 
 
 
