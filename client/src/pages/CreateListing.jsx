b71ce7ecb3f3578b50a162bb301191b347028cd9
Devesh Pandey <deveshpandey607@gmail.com>
2024-05-02 13:14:12 +0530

    create get user listing api route

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
 import { useSelector } from 'react-redux';
-import { useNavigate  }from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateListing() {
 
