9e7bfb945d2698dc439eb84fe5cc36fb1f42861f
Devesh Pandey <deveshpandey607@gmail.com>
2024-05-15 12:51:26 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -67,3 +67,59 @@ export const updateListing = async (req, res, next) => {
       next(error);
     }
   }
+
+  // search api
+  export const getListings = async (req, res, next) => {
+    try {
+
+      const limit = parseInt(req.query.limit) || 9;
+      const startIndex = parseInt(req.query.startIndex) || 0;
+
+      let offer = req.query.offer;
+
+      if (offer === undefined || offer === 'false') {
+        offer = { $in: [false, true] }; // this means show all related listing with and without offer
+      }
+
+     
+      let furnished = req.query.furnished;
+
+      if (furnished === undefined || furnished === 'false') {
+        furnished = { $in: [false, true] }; // this means show all related listing with and without furnished
+      }
+
+      let parking = req.query.parking;
+
+      if (parking === undefined || parking === 'false') {
+        parking = { $in: [false, true] }; // this means show all related listing with and without parking
+      }
+
+      let type = req.query.type;
+
+      if (type === undefined || type === 'all') {
+        type = { $in: ['sale', 'rent'] }; // this means show all related listing with and without parking
+      }
+
+      const searchTerm = req.query.searchTerm || '';
+
+      const sort = req.query.sort || 'createdAt';
+
+      const order = req.query.order || 'desc';
+
+      const listings = await Listing.find({
+        name: { $regex: searchTerm, $options: 'i' }, // regex line means search for any term used without caring lower,upper case ie.(i) used for that
+        offer,
+        furnished,
+        parking,
+        type,
+      }).sort(
+        {[sort]: order}
+      ).limit(limit).skip(startIndex);
+
+      return res.status(200).json(listings);
+      
+    } catch (error) {
+      next(error);
+    }
+
+  }
